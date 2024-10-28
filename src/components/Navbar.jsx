681d51e3e9888b706ba0d422fdece9048b55a6bd
sahib-singh13 <devsahibsingh@gmail.com>
2024-10-28 19:08:24 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>HOME PAGE HERE !!!!</div>
+  )
+}
+
+export default Home
