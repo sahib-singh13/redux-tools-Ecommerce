430cccbaf9501b8a5cda177a4f6c311ff01c41e3
sahib-singh13 <devsahibsingh@gmail.com>
2024-10-28 19:51:24 +0530

    made the navbar now turn for product and Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className='flex flex-row justify-between bg-sky-900 h-20'>
+      <NavLink to="/">
+        <div className="w-40 h-40 mt-4 mx-5">
+          <img src="/logo.png" alt="Logo" />
+        </div>
+      </NavLink>
+      <div className='flex flex-row  mx-12 mt-4'>
+        <NavLink to="/" className="mr-8 font-bold text-3xl relative mt-2">Home</NavLink> {/* Adjusted margin here */}
+        <NavLink to="/cart">
+          <img src="/favicon.ico" alt="Cart" />
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
