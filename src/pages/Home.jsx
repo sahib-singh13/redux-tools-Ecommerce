6bcc3af78da96b757134bdaff2581979b9e46fd5
sahib-singh13 <devsahibsingh@gmail.com>
2024-10-29 17:19:45 +0530

    successfully created the home page and the fetch page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Spinner from "../components/Spinner";
+import Product from '../components/Product';
 
 const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [current, setCurrent] = useState([]);
 
-  const API_URL="https://fakestoreapi.com/products";
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setData(data);
+      setCurrent(Array(data.length).fill(0)); 
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const toggleCart = (id) => {
+    setCurrent((prevCurrent) => {
+      const updatedCurrent = [...prevCurrent];
+      updatedCurrent[id] = updatedCurrent[id] === 1 ? 0 : 1;
+      return updatedCurrent;
+    });
+  };
+
   return (
-    <div>HOME PAGE HERE !!!!</div>
-  )
-}
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        data.map((item) => (
+          <Product key={item.id} {...item} current={current} toggleCart={toggleCart} />
+        ))
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
