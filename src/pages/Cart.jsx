c346711a828a6f53d7ffd4f2db9d8aad1f33ec12
sahib-singh13 <devsahibsingh@gmail.com>
2024-10-30 13:21:38 +0530

    done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import CartItem from '../components/CartItem'
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { cart } = useSelector((state) => state);
 
-export default Cart
+  // Calculate total cost using a normal loop
+  let totalCost = 0;
+  for (let item of cart) {
+    totalCost += item.price;
+  }
+
+  return (
+    <div>
+      {cart.length === 0 ? (
+        <div>
+          Your Cart is currently empty
+          <br />
+          <p>Click on Home button to add items</p>
+          <NavLink to="/">
+            <button>Home</button>
+          </NavLink>
+        </div>
+      ) : (
+        <div>
+          {cart.map((data) => (
+            <CartItem key={data.id} {...data} />
+          ))}
+          <div>
+            <span>Your Cart</span>
+            <span>SUMMARY</span>
+            <span>
+              Total Items: <span>{cart.length}</span>
+            </span>
+            <span>
+              Total Amount: <span>{`$${totalCost}`}</span>
+            </span>
+            <button>Check Out Now</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
